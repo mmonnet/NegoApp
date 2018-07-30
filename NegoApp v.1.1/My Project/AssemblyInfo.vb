2c48dbe19ca8adfc1944bca5a7395b81c969fb6a
mmonnet <41928357+mmonnet@users.noreply.github.com>
2018-07-31 00:28:01 +0200

    Add files via upload

---
+++ NegoApp v.1.1/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Passez en revue les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("NegoApp")> 
+<Assembly: AssemblyDescription("Programme de négociation statique et dynamique réalisé dans le cadre du Master en Relations Internationales de la Faculté de Philosophie et Sciences sociales de l'ULB, année 2017-2018. Promoteur M. Matteo Gagliolo.")> 
+<Assembly: AssemblyCompany("Réalisé par Melchior Monnet.")> 
+<Assembly: AssemblyProduct("NegoApp")> 
+<Assembly: AssemblyCopyright("Creative Commons BY-NC 4.0")> 
+<Assembly: AssemblyTrademark("NegoApp")> 
+
+<Assembly: ComVisible(False)> 
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("0cae015c-674a-4412-b3f2-d806bf47e986")> 
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.6")> 
+<Assembly: AssemblyFileVersion("1.0.0.6")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("fr-BE")> 
